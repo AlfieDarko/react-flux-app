a8015642f19c0232a08abebd7992e79286b41f17
Alfie Darko <mrhazzy@gmail.com>
2017-05-15 23:56:44 +0100

    Created the about page q q :wq

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class About extends Component{
+  render(){
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application uses the following technologies
+          <ul>
+            <li>React</li>
+            <li>React Router</li>
+            <li>Flux</li>
+            <li>Node</li>
+            <li>Gulp</li>
+            <li>Browserify</li>
+            <li>Bootstrap</li>
+          </ul>
+        </p>
+      </div>
+    )
+  }
+}
+export default About
--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Home from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
+import $ from "jquery";
+import About from './components/about/aboutPage'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class App extends component{
+  render(){
+    let Child
+
+    switch (this.props.route){
+      case 'about':
+      Child = About;
+      break;
+
+      default:
+      Child = Home;
+    }
+    return (
+      <div>
+        <Child />
+      </div>
+    );
+  }
+}
+
+  ReactDOM.render(
+    let route = window.location.hash.substr(1)
+    <App route={route} />,
+    document.getElementById('root')
+  );
+
+window.addEventListener('hashchange', render);
+render()
